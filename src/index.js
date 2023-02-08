f5e178d257c3aa62f579a3db8a19450a278826e3
Alexandre <aleaxe7@gmail.com>
2023-02-08 17:44:32 -0300

    Setup express initial server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+db.on("error", (error) => console.log(error))
+db.once("open", () => console.log("Connected to the Database!"))
+
+app.use(express.json())
+
+app.listen(3000, () => console.log("Server started!"))
